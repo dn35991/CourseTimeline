1a1207a4fec1a7271553e66a1e84e856960b788e
dn35991 <dharrshan-5@gmail.com>
2024-10-28 01:13:04 -0400

    removing personal.py contents

--- personal.py
+++ personal.py
@@ -1,8 +1,8 @@
 # Edit this value with the host name of your MySQL connection
-HOST_NAME = "localhost"
+HOST_NAME = ""
 
 # Edit this value with the username of your MySQL connection
-USERNAME = "root"
+USERNAME = ""
 
 # Edit this value with the password of your MySQL connection
-PASSWORD = "February@0205"
+PASSWORD = ""
