34b008148f3cb6ecf42cc8b36a4e42f8b5fd8445
dn35991 <dharrshan-5@gmail.com>
2024-10-28 01:30:06 -0400

    deleting personal

--- personal.py
+++
@@ -1,7 +0,0 @@
-# Edit this value with the host name of your MySQL connection
-HOST_NAME = "lllll"
-# Edit this value with the username of your MySQL connection
-USERNAME = ""
-
-# Edit this value with the password of your MySQL connection
-PASSWORD = ""
